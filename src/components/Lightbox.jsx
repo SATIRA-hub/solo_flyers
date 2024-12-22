c3ee3f2f974e7e67a64f7b778b35fecfdae43f4c
astrosuka <yegresvoselok@gmail.com>
2024-12-22 16:05:18 -0300

    Move close function to parent div and clean up

--- src/components/Lightbox.jsx
+++ src/components/Lightbox.jsx
@@ -1,17 +1,13 @@
 export default function Lightbox({ flyer, setSelectedFlyer }) {
   return (
-    <div className="no-doc-scroll fixed left-0 top-0 z-30 flex h-full w-full items-center justify-center bg-black bg-opacity-80">
-      {/* <div
-        className="fixed right-4 top-4 z-50 cursor-pointer select-none rounded-full border border-black bg-[#0f0] px-2 text-xl text-black"
-        onClick={() => setSelectedFlyer(null)}
-      >
-        X
-      </div> */}
+    <div
+      onClick={() => setSelectedFlyer(null)}
+      className="no-doc-scroll fixed left-0 top-0 z-30 flex h-full w-full cursor-zoom-out items-center justify-center bg-black bg-opacity-80"
+    >
       <img
         src={flyer.asset.url}
         alt={flyer.alt}
-        className="max-h-full max-w-full cursor-zoom-out"
-        onClick={() => setSelectedFlyer(null)}
+        className="max-h-full max-w-full"
       />
     </div>
   );
