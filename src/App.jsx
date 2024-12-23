57d80d9ecb48a3c57493ba5fa4e070d5c951815b
astrosuka <yegresvoselok@gmail.com>
2024-12-23 13:51:34 -0300

    Change title position on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
     <>
       <motion.div
         drag
-        className="fixed top-0 w-max cursor-move select-none border border-black bg-[#00ff00] px-1 text-center text-6xl font-black uppercase text-black mix-blend-difference sm:left-[calc(50%)] sm:top-[calc(45%)] sm:text-5xl md:sm:pb-1 lg:text-7xl"
+        className="fixed top-[calc(20%)] w-max cursor-move select-none border border-black bg-[#00ff00] px-1 text-center text-6xl font-black uppercase text-black mix-blend-difference sm:left-[calc(50%)] sm:top-[calc(45%)] sm:text-5xl md:sm:pb-1 lg:text-7xl"
       >
         <span className="text-[#000] blur-[2px] transition-all hover:blur-none">
           <span className="inline-block font-[Sniglet]">solo</span>
